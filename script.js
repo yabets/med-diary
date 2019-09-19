80cf4cc7e5107813f66f4d2dbc6cc3fc2ae70534
aben-bel <abenezer.sbelay@gmail.com>
2019-09-19 10:46:21 +0300

    add edit, save and create form

--- script.js
+++ script.js
@@ -1,104 +1,125 @@
-// ##################################################### //
 
-// helper functions
+(() => {
+  // ##################################################### //
 
-const removeClass = (node, name) => {
-  node.classList.remove(name);
-};
+  // helper functions
 
-const addClass = (node, name) => {
-  node.classList.add(name);
-};
+  const removeClass = (node, name) => {
+    node.classList.remove(name);
+  };
 
-const showClass = (...nodes) => nodes.forEach((node) => removeClass(node, 'hidden'));
+  const addClass = (node, name) => {
+    node.classList.add(name);
+  };
 
-
-const hideClass = (...nodes) => nodes.forEach((node) => addClass(node, 'hidden'));
-
-// ##################################################### //
+  const showClass = (...nodes) => nodes.forEach((node) => removeClass(node, 'hidden'));
 
 
-const newDiary = document.querySelector('#newDiary');
+  const hideClass = (...nodes) => nodes.forEach((node) => addClass(node, 'hidden'));
 
-const newForm = () => {
+  // ##################################################### //
+  let currentLi;
+  const newDiary = document.querySelector('#newDiary');
+  const form = document.querySelector('#create');
   const ulForRecords = document.querySelector('#ul-for-records');
-  const listRecordContainer = document.querySelector('#list-records-container');
-  const form = document.querySelector('#form');
+
+  const edit = (evt) => {
+    // show form and set the currentLi value
+    form.classList.remove('hide');
+    currentLi = evt.srcElement;
+  };
+
+  const newForm = () => {
+    const listRecordContainer = document.querySelector('#list-records-container');
+    const detail = document.querySelector('#detail');
+
+    // create one list
+    const li = document.createElement('LI');
+    const div = document.createElement('DIV');
+    const span = document.createElement('SPAN');
+
+    const liCopy = li.cloneNode();
+    liCopy.classList.add('card');
+    const divCopy = div.cloneNode();
+    divCopy.classList.add('flex-columns');
+
+    const divCopy1 = div.cloneNode();
+    divCopy1.classList.add('row', 'spread');
+    const spanCopy = span.cloneNode();
+    spanCopy.classList.add('subject');
+    const spanCopy1 = span.cloneNode();
+    spanCopy1.classList.add('timestamp', 'float-right');
+    divCopy1.appendChild(spanCopy);
+    divCopy1.appendChild(spanCopy1);
+    divCopy.appendChild(divCopy1);
+
+    const divCopy2 = div.cloneNode();
+    divCopy2.classList.add('row');
+    const spanCopy2 = span.cloneNode();
+    spanCopy2.classList.add('highlight');
+    divCopy2.appendChild(spanCopy2);
+    divCopy.appendChild(divCopy2);
+
+    liCopy.appendChild(divCopy);
+    ulForRecords.appendChild(liCopy);
+
+
+    liCopy.addEventListener('click', edit);
+
+    // show form and set the currentLi value
+    form.classList.remove('hide');
+    currentLi = ulForRecords.lastElementChild;
+    console.log('li from add: ', currentLi);
+
+
+    // check size of browser and render accordingly
+    const mediaQuery = window.matchMedia('(max-width: 700px)'); // on mobile
+    if (mediaQuery.matches) {
+      hideClass(listRecordContainer);
+      showClass(detail);
+    } else {
+      showClass(listRecordContainer);
+      showClass(detail);
+    }
+  };
+
+  newDiary.addEventListener('click', newForm);
+
+
   const save = document.querySelector('#create-submit');
 
+  const recordInList = (values) => {
+    const { subject, description, date } = values;
+    const sub = currentLi.querySelector('.subject');
+    const time = currentLi.querySelector('.timestamp');
+    const desc = currentLi.querySelector('.highlight');
 
-  // create one list
-  const li = document.createElement('LI');
-  const div = document.createElement('DIV');
-  const span = document.createElement('SPAN');
+    sub.textContent = `Subject: ${subject}`;
+    time.textContent = date;
+    desc.textContent = description;
 
-  const liCopy = li.cloneNode();
-  liCopy.classList.add('card');
-  const divCopy = div.cloneNode();
-  divCopy.classList.add('flex-columns');
-
-  const divCopy1 = div.cloneNode();
-  divCopy1.classList.add('row', 'spread');
-  const spanCopy = span.cloneNode();
-  spanCopy.classList.add('subject');
-  const spanCopy1 = span.cloneNode();
-  spanCopy1.classList.add('timestamp', 'float-right');
-  divCopy1.appendChild(spanCopy);
-  divCopy1.appendChild(spanCopy1);
-  divCopy.appendChild(divCopy1);
-
-  const divCopy2 = div.cloneNode();
-  divCopy2.classList.add('row');
-  const spanCopy2 = span.cloneNode();
-  spanCopy2.classList.add('highlight');
-  divCopy2.appendChild(spanCopy2);
-  divCopy.appendChild(divCopy2);
-
-  liCopy.appendChild(divCopy);
-  ulForRecords.appendChild(liCopy);
-
-
-  // check size of browser and render accordingly
-  const mediaQuery = window.matchMedia('(max-width: 700px)'); // on mobile
-  if (mediaQuery.matches) {
-    hideClass(listRecordContainer);
-    showClass(form);
-  } else {
-    showClass(listRecordContainer);
-    showClass(form);
-  }
+    form.classList.add('hide');
+  };
 
   // callback for save button for form
-  const acceptFrom = () => {
+  const saveForm = () => {
     const subject = document.querySelector('#subject');
     const description = document.querySelector('#description');
     const date = document.querySelector('#date');
-
-    const lastLi = ulForRecords.lastElementChild;
 
     const subjectValue = subject.value;
     const descriptionValue = description.value;
     const dateValue = date.value;
 
     if (subjectValue !== '' && descriptionValue !== '' && dateValue !== '') {
-      // const span
-      const sub = lastLi.querySelector('.subject');
-      const time = lastLi.querySelector('.timestamp');
-      const desc = lastLi.querySelector('.highlight');
-
-      sub.textContent = `Subject ${subjectValue}`;
-      time.textContent = dateValue;
-      desc.textContent = descriptionValue;
-
-      hideClass(form);
-      showClass(listRecordContainer);
+      subject.value = '';
+      description.value = '';
+      date.value = '';
+      recordInList({ subject: subjectValue, description: descriptionValue, date: dateValue });
     } else {
       alert('wrong inputs');
     }
   };
 
-
-  save.addEventListener('click', acceptFrom);
-};
-
-newDiary.addEventListener('click', newForm);
+  save.addEventListener('click', saveForm);
+})();
